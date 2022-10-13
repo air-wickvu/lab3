24f7bb7199485f037ce77fad26738c399c0d209c
Eric Vu <56463764+air-wickvu@users.noreply.github.com>
2022-10-13 16:56:32 -0700

    update

--- ListsTests.java
+++ ListsTests.java
@@ -5,11 +5,18 @@ import java.util.*;
 public class ListsTests {
 	@Test 
 	public void testMerge() {
-    List<String> input1 = Arrays.asList("sup1","sup2","sup3");
-    List<String> input2 = Arrays.asList("1","2","3");
+    List<String> input1 = Arrays.asList("a","b","c");
+    List<String> input2 = Arrays.asList("d","e","f");
 
-    ListExamples.merge(input1, input2);
+   List<String> result = ListExamples.merge(input1, input2);
+   //System.out.println(result);
+   List<String> actual = Arrays.asList("a","b","c","d","e","f");
+   Assert.assertEquals(result, actual);
 
-    //assertArrayEquals(new int[]{ 3 }, input1);
+   //assertTrue(result.size() == actual.size() && result.containsAll(second) && actual.containsAll(first));
 	}
 }
+
+//can a failure be when we pass an unsorted array. 
+//javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
+//java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore ListsTests
