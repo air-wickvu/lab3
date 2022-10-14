9a32507cbe8839712c94092e9028dfd3e0d11ef4
Eric Vu <56463764+air-wickvu@users.noreply.github.com>
2022-10-14 12:46:23 -0700

    update

--- ListsTests.java
+++ ListsTests.java
@@ -1,18 +1,30 @@
 import static org.junit.Assert.*;
 import org.junit.*;
-import java.util.*; 
+import java.util.*;
 
 public class ListsTests {
-	@Test 
+	@Test(timeout=500)
 	public void testMerge() {
-    List<String> input1 = Arrays.asList("a","b","c");
-    List<String> input2 = Arrays.asList("d","e","f");
+    //input
+    //String[] inputChar = new String[]{"a","b","c"};
+    //String[] inputChar2 = new String[]{"d","e","f"};
 
+    String[] inputChar3 = new String[]{"a"};
+    String[] inputChar4 = new String[]{"b"};
+
+
+    
+    List<String> input1 = Arrays.asList(inputChar3);
+    List<String> input2 = Arrays.asList(inputChar4);
+
+    //action 
    List<String> result = ListExamples.merge(input1, input2);
    //System.out.println(result);
-   List<String> actual = Arrays.asList("a","b","c","d","e","f");
-   Assert.assertEquals(result, actual);
+   //test 
+   String[] expected = new String[]{"a","b","c","d","e","f"};
+   List<String> actual = Arrays.asList(expected);
 
+   Assert.assertEquals(result,actual);
    //assertTrue(result.size() == actual.size() && result.containsAll(second) && actual.containsAll(first));
 	}
 }
